5f9b0931a2893c848e19454edb654e31e9d55e24
MugeraH <bwosihughes24@gmail.com>
2021-02-28 01:33:51 +0300

    add javascript function to toggle card-show and card-hidden cards

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  let cardTotalNumber =(
+      $(".card").toArray().length
+    );
+
+  for(let cardNumber = 1; cardNumber<=cardTotalNumber;cardNumber++ ){
+      $(`.card${cardNumber}`).click(function() {
+          $(`.card-show${cardNumber}`).toggle(400);
+          $(`.card-hidden${cardNumber}`).toggle(400);
+        });
+    }
+  
+
+    let overlayTotalNumber =(
+      $(".overlay").toArray().length
+    );
+
+    for(let overlayNumber = 1; overlayNumber<=overlayTotalNumber;overlayNumber++){
+        $(`#overlay${overlayNumber}`).mouseover(function() {
+        $(`#overlay${overlayNumber} > img`).addClass("overlay-effect");
+      }).mouseout(function(){
+        $(`#overlay${overlayNumber} > img`).removeClass("overlay-effect");
+      });
+    }
+  
+  
+  
+  
+  });
+  
+  
+  
+  
+  
