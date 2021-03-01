f1ef1eeeb79d4283f14f35ef82c6c61e8ee92980
MugeraH <bwosihughes24@gmail.com>
2021-03-01 15:56:39 +0300

    fix mailchip subscribe request

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,21 @@ $(document).ready(function() {
 
 
   $("form#mc-embedded-subscribe-form").submit(function(event) {
-  //event.preventDefault();
-    let name = $("input#FNAME").val();
-    let email = $("input#mce-Email").val();
+  event.preventDefault();
+    let name = $("input#mce-FNAME").val();
+    let email = $("input#mce-EMAIL").val();
     let message = $("textarea").val();
-    if ( name && email){
-      alert (`Hi ${name} we have received your message. Thank you for reaching out to us.`);
-    }
+
+
+    if ( name && email && message){
+          alert (`Hi ${name} we have received your message. Thank you for reaching out to us.`);
+      }
     else {
       alert("Please enter your name and email!");
-    }
+        }
   });
 
+  
   let cardTotalNumber =(
       $(".card").toArray().length
     );
@@ -38,11 +41,7 @@ $(document).ready(function() {
         $(`#overlay${overlayNumber} > img`).removeClass("overlay-effect");
         $(`#work-overlay${overlayNumber}`).toggle();
       });
-    }
-  
-  
-  
-  
+    }  
   });
   
   
